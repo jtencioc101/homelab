bf1b4818267357cf9b779e9fb6cffcd43fa414c2
Julio Tencio <j.tenciocoto@gmail.com>
2025-02-24 09:37:46 -0600

    cleaning up

--- talosctl/setup.sh
+++ talosctl/setup.sh
@@ -5,14 +5,14 @@ talosctl gen config talos-proxmox-cluster https://172.16.10.5:6443 --output-dir 
 talosctl get disks --insecure --nodes 172.16.10.5
 
 # Create Control Plane Node
-talosctl apply-config --insecure --nodes 172.16.10.5 --file _out/controlplane.yaml --force
+talosctl apply-config --insecure --nodes 172.16.10.5 --file _out/controlplane.yaml
 
 talosctl gen config talos-proxmox-cluster https://172.16.10.5:6443 --output-dir _out --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.2
 
 
 # Create Worker Nodes
-talosctl apply-config --insecure --nodes 172.16.10.6 --file _out/worker.yaml --force
-talosctl apply-config --insecure --nodes 172.16.10.7 --file _out/worker.yaml --force
+talosctl apply-config  --insecure --nodes 172.16.10.6 --file _out/worker.yaml
+talosctl apply-config  --insecure --nodes 172.16.10.7 --file _out/worker.yaml
 
 
 # Using the Cluster
