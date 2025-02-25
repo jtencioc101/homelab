afb3195b2a57f1b7bb494aa6f81fd745718c08aa
Julio Tencio <j.tenciocoto@gmail.com>
2025-02-25 10:58:37 -0600

    restoring talosctl bootstrap line

--- talosctl/setup.sh
+++ talosctl/setup.sh
@@ -7,8 +7,7 @@ talosctl get disks --insecure --nodes 172.16.10.5
 # Create Control Plane Node
 talosctl apply-config --insecure --nodes 172.16.10.5 --file _out/controlplane.yaml
 
-talosctl gen config talos-proxmox-cluster https://172.16.10.5:6443 --output-dir _out --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.2
-
+talosctl gen config talos-proxmox-cluster https://172.16.10.5:6443 --output-dir _out --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.4
 
 # Create Worker Nodes
 talosctl apply-config  --insecure --nodes 172.16.10.6 --file _out/worker.yaml
@@ -22,6 +21,8 @@ talosctl config node 172.16.10.5
 
 # Must wait for reebot
 sleep 60
+
+talosctl bootstrap
 
 talosctl kubeconfig /Users/julio/.kubeconfig 
 
