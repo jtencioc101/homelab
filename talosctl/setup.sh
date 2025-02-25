d489eab6db2508c6f0be1676e232675aad3611c8
Julio Tencio <j.tenciocoto@gmail.com>
2025-02-25 09:10:08 -0600

    cleanup

--- talosctl/setup.sh
+++ talosctl/setup.sh
@@ -23,18 +23,6 @@ talosctl config node 172.16.10.5
 # Must wait for reebot
 sleep 60
 
-talosctl bootstrap && helm repo add cilium https://helm.cilium.io/ && helm repo update && helm install \
-    cilium \
-    cilium/cilium \
-    --version 1.15.6 \
-    --namespace kube-system \
-    --set ipam.mode=kubernetes \
-    --set kubeProxyReplacement=false \
-    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
-    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
-    --set cgroup.autoMount.enabled=false \
-    --set cgroup.hostRoot=/sys/fs/cgroup
-
 talosctl kubeconfig /Users/julio/.kubeconfig 
 
 # Set kubeconfig file
