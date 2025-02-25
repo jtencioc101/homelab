9967984779a21f257ea4fe63cd3579dcd927ce07
Julio Tencio <j.tenciocoto@gmail.com>
2025-02-25 11:50:36 -0600

    removing values file, not needed at this point

--- k8s-manifests/traefik/traefik.sh
+++ k8s-manifests/traefik/traefik.sh
@@ -4,5 +4,4 @@ kubectl create ns traefik-v2
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
 helm install --namespace=traefik-v2 \
-	--values traefik-values.yaml \
 	traefik traefik/traefik
