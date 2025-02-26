940523b378453493ad2e1d3e6205cf66fbf17074
Julio Tencio <j.tenciocoto@gmail.com>
2025-02-26 09:45:33 -0600

    fixing ip addresses

--- talosctl/setup.sh
+++ talosctl/setup.sh
@@ -1,23 +1,23 @@
 !#/bin/bash
 
 # Generate Machine Configurations
-talosctl gen config --config-patch @patch.yaml talos-proxmox-cluster https://172.16.10.5:6443 --output-dir _out
-talosctl get disks --insecure --nodes 172.16.10.5
+talosctl gen config --config-patch @patch.yaml talos-proxmox-cluster https://172.16.10.16:6443 --output-dir _out
+talosctl get disks --insecure --nodes 172.16.10.16
 
 # Create Control Plane Node
-talosctl apply-config --insecure --nodes 172.16.10.5 --file _out/controlplane.yaml
+talosctl apply-config --insecure --nodes 172.16.10.16 --file _out/controlplane.yaml
 
-talosctl gen config talos-proxmox-cluster https://172.16.10.5:6443 --output-dir _out --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.4
+talosctl gen config talos-proxmox-cluster https://172.16.10.16:6443 --output-dir _out --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.4
 
 # Create Worker Nodes
-talosctl apply-config  --insecure --nodes 172.16.10.6 --file _out/worker.yaml
-talosctl apply-config  --insecure --nodes 172.16.10.7 --file _out/worker.yaml
+talosctl apply-config  --insecure --nodes 172.16.10.17 --file _out/worker.yaml
+talosctl apply-config  --insecure --nodes 172.16.10.18 --file _out/worker.yaml
 
 
 # Using the Cluster
 export TALOSCONFIG="_out/talosconfig" 
-talosctl config endpoint 172.16.10.5
-talosctl config node 172.16.10.5
+talosctl config endpoint 172.16.10.16
+talosctl config node 172.16.10.16
 
 # Must wait for reebot
 sleep 60
