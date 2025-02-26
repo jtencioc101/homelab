2801387704a5c1a693053c6cb73a1cb306750312
Julio Tencio <j.tenciocoto@gmail.com>
2025-02-26 11:53:22 -0600

    cleanup

--- talosctl/setup.sh
+++ talosctl/setup.sh
@@ -1,25 +1,28 @@
 !#/bin/bash
 
 # Generate Machine Configurations
+
 talosctl gen config --config-patch @patch.yaml talos-proxmox-cluster https://172.16.10.16:6443 --output-dir _out
 talosctl get disks --insecure --nodes 172.16.10.16
 
 # Create Control Plane Node
+
 talosctl apply-config --insecure --nodes 172.16.10.16 --file _out/controlplane.yaml
 
-talosctl gen config talos-proxmox-cluster https://172.16.10.16:6443 --output-dir _out --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.9.4
-
 # Create Worker Nodes
+
 talosctl apply-config  --insecure --nodes 172.16.10.17 --file _out/worker.yaml
 talosctl apply-config  --insecure --nodes 172.16.10.18 --file _out/worker.yaml
 
 
 # Using the Cluster
+
 export TALOSCONFIG="_out/talosconfig" 
 talosctl config endpoint 172.16.10.16
 talosctl config node 172.16.10.16
 
 # Must wait for reebot
+
 sleep 60
 
 talosctl bootstrap
@@ -27,4 +30,5 @@ talosctl bootstrap
 talosctl kubeconfig /Users/julio/.kubeconfig 
 
 # Set kubeconfig file
+
 export KUBECONFIG=/Users/julio/.kubeconfig
